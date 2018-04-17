13a3fd5b6baa60f539a5a622ed1cee7ca6687842
Corey Emery <coreyemery@Coreys-MBP.wv.cc.cmu.edu>
2018-04-17 15:25:55 -0400

    Implemented `run_simulation`, wrote the loops to create all three plots, and answered the question posed in the write-up

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -13,3 +13,24 @@ model_select <- function(covariates, response, cutoff) {
     return (summary(model_reduced)$coefficients[-1, "Pr(>|t|)"])
   }
 }
+
+run_simulation <- function(n_trials, n, p, cutoff) {
+  result <- vector()
+  for (i in 1:n_trials) {
+    data <- generate_data(n, p)
+    result <- c(result, model_select(data$covariates, data$response, cutoff))
+  }
+  hist(result)
+  return ()
+}
+
+par(mfrow = c(3,3))
+for (n in c(100, 1000, 10000)) {
+  for (p in c(10, 20, 50)) {
+    run_simulation(100, n, p, 0.05)
+  }
+}
+
+# It does not appear to be the case that the p-values are uniformly distributed
+# between 0 and 1 (probably because we filtered out every p-value that was 
+# greater than 0.05 in the original regression model)
